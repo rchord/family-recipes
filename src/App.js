b08952242a46760ba039cd4400763de5a2647906
Rylan Chord <rchord@alianza.com>
2018-01-21 21:17:22 -0700

    Messed around with react basics

--- src/App.js
+++ src/App.js
@@ -3,19 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    renderFamily(i) {
+        return <Family value={i}/>;
+    }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Family Recipes</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <div>
+           {this.renderFamily('Rob')}
+          </div>
+          <div>
+           {this.renderFamily('Rob')}
+          </div>
+          <div>
+           {this.renderFamily('Rob')}
+          </div>
       </div>
     );
   }
+}
+
+class Family extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    render() {
+        return (
+          <button className="Family" onClick={() => this.setState({value: 'Hello'})}>
+              {this.state.value}
+          </button>
+        );
+    }
 }
 
 export default App;
